b7834a9fa159a6755268603f6d7cd8b8b15cf424
btt-17 <btthanh.46@gmail.com>
2023-01-18 22:07:35 +0900

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+import TokyoModel from '../components/tokyo-model'
+
+const Home: NextPage = () => {
+
+  return (
+   <>
+      <TokyoModel></TokyoModel>
+    </>
+  )
+}
+
+export default Home
