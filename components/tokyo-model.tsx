c244861db049b353faff62fac9ada0dd4e79e67b
btt-17 <btthanh.46@gmail.com>
2023-01-18 22:31:35 +0900

    update README and refactoring

--- components/tokyo-model.tsx
+++ components/tokyo-model.tsx
@@ -13,83 +13,83 @@ import path from 'path'
 
 const TokyoModel = () => {
   useEffect(() => {
-    let mixer;
+	let mixer;
 
-			const clock = new THREE.Clock();
-			const container = document.getElementById( 'container' );
+	const clock = new THREE.Clock();
+	const container = document.getElementById( 'container' );
 
-			const stats = new Stats();
-			container.appendChild( stats.dom );
+	const stats = new Stats();
+	container.appendChild( stats.dom );
 
-			const renderer = new THREE.WebGLRenderer( { antialias: true } );
-			renderer.setPixelRatio( window.devicePixelRatio );
-			renderer.setSize( window.innerWidth, window.innerHeight );
-			renderer.outputEncoding = THREE.sRGBEncoding;
-			container.appendChild( renderer.domElement );
+	const renderer = new THREE.WebGLRenderer( { antialias: true } );
+	renderer.setPixelRatio( window.devicePixelRatio );
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	renderer.outputEncoding = THREE.sRGBEncoding;
+	container.appendChild( renderer.domElement );
 
-			const pmremGenerator = new THREE.PMREMGenerator( renderer );
+	const pmremGenerator = new THREE.PMREMGenerator( renderer );
 
-			const scene = new THREE.Scene();
-			scene.background = new THREE.Color( 0xbfe3dd );
-			scene.environment = pmremGenerator.fromScene( new RoomEnvironment(), 0.04 ).texture;
+	const scene = new THREE.Scene();
+	scene.background = new THREE.Color( 0xbfe3dd );
+	scene.environment = pmremGenerator.fromScene( new RoomEnvironment(), 0.04 ).texture;
 
-			const camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 100 );
-			camera.position.set( 5, 2, 8 );
+	const camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 100 );
+	camera.position.set( 5, 2, 8 );
 
-			const controls = new OrbitControls( camera, renderer.domElement );
-			controls.target.set( 0, 0.5, 0 );
-			controls.update();
-			controls.enablePan = false;
-			controls.enableDamping = true;
-      
-      const dracoLoader = new DRACOLoader();
-			dracoLoader.setDecoderPath( 'jsm/libs/draco/gltf/' );
+	const controls = new OrbitControls( camera, renderer.domElement );
+	controls.target.set( 0, 0.5, 0 );
+	controls.update();
+	controls.enablePan = false;
+	controls.enableDamping = true;
 
-			const loader = new GLTFLoader();
-      loader.setDRACOLoader( dracoLoader );
-			loader.load('models/gltf/LittlestTokyo.glb', function ( gltf ) {
-        
-				const model = gltf.scene;
-				model.position.set( 1, 1, 0 );
-				model.scale.set( 0.01, 0.01, 0.01 );
-				scene.add( model );
+	const dracoLoader = new DRACOLoader();
+	dracoLoader.setDecoderPath( 'jsm/libs/draco/gltf/' );
 
-				mixer = new THREE.AnimationMixer( model );
-				mixer.clipAction( gltf.animations[ 0 ] ).play();
+	const loader = new GLTFLoader();
+	loader.setDRACOLoader( dracoLoader );
+	loader.load('models/gltf/LittlestTokyo.glb', function ( gltf ) {
 
-				animate();
+		const model = gltf.scene;
+		model.position.set( 1, 1, 0 );
+		model.scale.set( 0.01, 0.01, 0.01 );
+		scene.add( model );
 
-			}, undefined, function ( error ) {
-				console.error( error );
+		mixer = new THREE.AnimationMixer( model );
+		mixer.clipAction( gltf.animations[ 0 ] ).play();
 
-			} );
+		animate();
 
+	}, undefined, function ( error ) {
+		console.error( error );
 
-			window.onresize = function () {
-
-				camera.aspect = window.innerWidth / window.innerHeight;
-				camera.updateProjectionMatrix();
-
-				renderer.setSize( window.innerWidth, window.innerHeight );
-
-			};
+	} );
 
 
-			function animate() {
+	window.onresize = function () {
 
-				requestAnimationFrame( animate );
+		camera.aspect = window.innerWidth / window.innerHeight;
+		camera.updateProjectionMatrix();
 
-				const delta = clock.getDelta();
+		renderer.setSize( window.innerWidth, window.innerHeight );
 
-				mixer.update( delta );
+	};
 
-				controls.update();
 
-				stats.update();
+	function animate() {
 
-				renderer.render( scene, camera );
+		requestAnimationFrame( animate );
 
-			}
+		const delta = clock.getDelta();
+
+		mixer.update( delta );
+
+		controls.update();
+
+		stats.update();
+
+		renderer.render( scene, camera );
+
+	}
 
   })
   return (
